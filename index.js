256ae50d8acedbc5e1bf7d3244a321060f9a1804
R Govaert <govaertreeven@gmail.com>
2022-12-12 08:39:39 +0100

    bruh why even, just format this

--- index.js
+++ index.js
@@ -1,169 +1,157 @@
 // This project is licensed with Roz's license, which should've been included within the root directory of this project, or - https://github.com/rozbrajaczpoziomow/coc-bot-electric-boogaloo/blob/main/LICENSE.txt
 // This line is a tribute to me leaking my CG token on the second commit on the repo on GH
-const AllConfig = require("./config.json");
-const tmi = require("tmi.js");
-require("child_process")
-  .spawn("git", ["pull", "-f"], {
-    shell: true,
-    windowsHide: true,
-    detached: true,
-  })
-  .unref();
+const AllConfig = require('./config.json');
+const tmi = require('tmi.js');
+require('child_process')
+	.spawn('git', ['pull', '-f'], {
+		shell: true,
+		windowsHide: true,
+		detached: true,
+	})
+	.unref();
 
 if (AllConfig.Twitch.evalGlobal)
-  for (var i = 0; i < 5; i++)
-    console.log(
-      "[!!!] Having evalGlobal enabled is a terrible idea... For token safety reasons or something idk..."
-    );
+	for (var i = 0; i < 5; i++)
+		console.log(
+			'[!!!] Having evalGlobal enabled is a terrible idea... For token safety reasons or something idk...'
+		);
 
 async function SaveConfig() {
-  const { writeFile } = require("fs");
-  writeFile("./config.json", JSON.stringify(AllConfig, null, 4), () => {});
+	const { writeFile } = require('fs');
+	writeFile('./config.json', JSON.stringify(AllConfig, null, 4), () => {});
 }
 
 Array.prototype.last = function last() {
-  return this[this.length - 1];
+	return this[this.length - 1];
 };
 
 const CG = {
-  Config: AllConfig.CodinGame,
-  Request: {
-    GET: async function GET(url) {
-      console.debug(`[CG  GET] ${url}`);
-      return fetch(url, {
-        method: "GET",
-        headers: {
-          Cookie: `cgSession=${CG.Config.token}`,
-        },
-      });
-    },
-    POST: async function POST(url, data) {
-      console.debug(`[CG POST] ${url} - `, data);
-      return fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type":
-            typeof data == "object"
-              ? "application/json"
-              : "application/x-www-url-encoded",
-          Cookie: `cgSession=${CG.Config.token}`,
-        },
-        body: typeof data == "object" ? JSON.stringify(data) : data,
-      });
-    },
-  },
-  Clash: class Clash {
-    modes;
-    languages;
-    publicHandle;
-    ideHandle;
+	Config: AllConfig.CodinGame,
+	Request: {
+		GET: async function GET(url) {
+			console.debug(`[CG	GET] ${url}`);
+			return fetch(url, {
+				method: 'GET',
+				headers: {
+					Cookie: `cgSession=${CG.Config.token}`,
+				},
+			});
+		},
+		POST: async function POST(url, data) {
+			console.debug(`[CG POST] ${url} - `, data);
+			return fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type':
+						typeof data == 'object'
+							? 'application/json'
+							: 'application/x-www-url-encoded',
+					Cookie: `cgSession=${CG.Config.token}`,
+				},
+				body: typeof data == 'object' ? JSON.stringify(data) : data,
+			});
+		},
+	},
+	Clash: class Clash {
+		modes;
+		languages;
+		publicHandle;
+		ideHandle;
 
-    static modeMapping = {
-      f: "FASTEST",
-      fast: "FASTEST",
-      fastest: "FASTEST",
-      s: "SHORTEST",
-      short: "SHORTEST",
-      shortest: "SHORTEST",
-      r: "REVERSE",
-      rev: "REVERSE",
-      ver: "REVERSE",
-      reverse: "REVERSE",
-      reversimus: "REVERSE",
-    };
+		static modeMapping = {f: 'FASTEST', fast: 'FASTEST', fastest: 'FASTEST', s: 'SHORTEST', short: 'SHORTEST', shortest: 'SHORTEST', r: 'REVERSE', rev: 'REVERSE', ver: 'REVERSE', reverse: 'REVERSE', reversimus: 'REVERSE',};
 
-    static languages = null;
-    static languagesLower = null;
+		static languages = null;
+		static languagesLower = null;
 
-    constructor(modes, langs) {
-      this.modes = new Set(
-        modes
-          .map((mode) => CG.Clash.modeMapping[mode] || "")
-          .filter((mode) => mode)
-      );
-      this.languages = new Set(
-        langs
-          .map((lang) =>
-            CG.Clash.languagesLower.includes(lang.toLowerCase())
-              ? CG.Clash.languages[
-                  CG.Clash.languagesLower.indexOf(lang.toLowerCase())
-                ]
-              : ""
-          )
-          .filter((lang) => lang)
-      );
-    }
+		constructor(modes, langs) {
+			this.modes = new Set(
+				modes
+					.map((mode) => CG.Clash.modeMapping[mode] || '')
+					.filter((mode) => mode)
+			);
+			this.languages = new Set(
+				langs
+					.map((lang) =>
+						CG.Clash.languagesLower.includes(lang.toLowerCase())
+							? CG.Clash.languages[
+									CG.Clash.languagesLower.indexOf(lang.toLowerCase())
+								]
+							: ''
+					)
+					.filter((lang) => lang)
+			);
+		}
 
-    async create() {
-      const req = await CG.Request.POST(
-        "https://www.codingame.com/services/ClashOfCode/createPrivateClash",
-        [CG.Config.userId, [...this.languages], [...this.modes]]
-      );
-      const json = await req.json();
+		async create() {
+			const req = await CG.Request.POST(
+				'https://www.codingame.com/services/ClashOfCode/createPrivateClash',
+				[CG.Config.userId, [...this.languages], [...this.modes]]
+			);
+			const json = await req.json();
 
-      if (req.status != 200) {
-        console.error(
-          `* [CG] Creating clash failed with code ${req.status}, server returned the following data:`
-        );
-        console.error(json);
-        return false;
-      }
+			if (req.status != 200) {
+				console.error(
+					`* [CG] Creating clash failed with code ${req.status}, server returned the following data:`
+				);
+				console.error(json);
+				return false;
+			}
 
-      this.publicHandle = json.publicHandle;
-      return true;
-    }
+			this.publicHandle = json.publicHandle;
+			return true;
+		}
 
-    async start() {
-      await CG.Request.POST(
-        "https://www.codingame.com/services/ClashOfCode/startClashByHandle",
-        [CG.Config.userId, this.publicHandle]
-      );
-    }
+		async start() {
+			await CG.Request.POST(
+				'https://www.codingame.com/services/ClashOfCode/startClashByHandle',
+				[CG.Config.userId, this.publicHandle]
+			);
+		}
 
-    async submit() {
-      console.log(`[CG] Submitting for clash ${this.publicHandle}`);
-      this.ideHandle = (
-        await (
-          await CG.Request.POST(
-            "https://www.codingame.com/services/ClashOfCode/startClashTestSession",
-            [CG.Config.userId, this.publicHandle]
-          )
-        ).json()
-      ).handle;
-      await CG.Request.POST(
-        "https://www.codingame.com/services/TestSession/submit",
-        [
-          this.ideHandle,
-          {
-            code: `Stream: https://twitch.tv/${Twitch.Config.channel}\nBot: https://github.com/rozbrajaczpoziomow/coc-bot-electric-boogaloo`,
-            programmingLanguageId:
-              CG.Clash.languages[
-                Math.floor(Math.random() * CG.Clash.languages.length)
-              ],
-          },
-          null,
-        ]
-      );
-      await CG.Request.POST(
-        "https://www.codingame.com/services/ClashOfCode/shareCodinGamerSolutionByHandle",
-        [CG.Config.userId, this.publicHandle]
-      );
-    }
+		async submit() {
+			console.log(`[CG] Submitting for clash ${this.publicHandle}`);
+			this.ideHandle = (
+				await (
+					await CG.Request.POST(
+						'https://www.codingame.com/services/ClashOfCode/startClashTestSession',
+						[CG.Config.userId, this.publicHandle]
+					)
+				).json()
+			).handle;
+			await CG.Request.POST(
+				'https://www.codingame.com/services/TestSession/submit',
+				[
+					this.ideHandle,
+					{
+						code: `Stream: https://twitch.tv/${Twitch.Config.channel}\nBot: https://github.com/rozbrajaczpoziomow/coc-bot-electric-boogaloo`,
+						programmingLanguageId:
+							CG.Clash.languages[
+								Math.floor(Math.random() * CG.Clash.languages.length)
+							],
+					},
+					null,
+				]
+			);
+			await CG.Request.POST(
+				'https://www.codingame.com/services/ClashOfCode/shareCodinGamerSolutionByHandle',
+				[CG.Config.userId, this.publicHandle]
+			);
+		}
 
-    get url() {
-      return `https://www.codingame.com/clashofcode/clash/${this.publicHandle}`;
-    }
+		get url() {
+			return `https://www.codingame.com/clashofcode/clash/${this.publicHandle}`;
+		}
 
-    static async init() {
-      this.languages = await (
-        await CG.Request.POST(
-          "https://www.codingame.com/services/ProgrammingLanguage/findAllIds",
-          []
-        )
-      ).json();
-      this.languagesLower = this.languages.map((x) => x.toLowerCase());
-    }
-  },
+		static async init() {
+			this.languages = await (
+				await CG.Request.POST(
+					'https://www.codingame.com/services/ProgrammingLanguage/findAllIds',
+					[]
+				)
+			).json();
+			this.languagesLower = this.languages.map((x) => x.toLowerCase());
+		}
+	},
 };
 
 CG.Clash.init();
@@ -171,144 +159,97 @@ CG.Clash.init();
 const Twitch = {};
 Twitch.Config = AllConfig.Twitch;
 Twitch.Client = new tmi.Client({
-  options: {
-    debug: true,
-  },
-  identity: {
-    username: Twitch.Config.username,
-    password: Twitch.Config.token,
-  },
-  channels: [Twitch.Config.channel],
+	options: {
+		debug: true,
+	},
+	identity: {
+		username: Twitch.Config.username,
+		password: Twitch.Config.token,
+	},
+	channels: [Twitch.Config.channel],
 });
 
-/**
- * Checks if user is admin
- *
- * @param {string} username
- * @returns {boolean} true if user is admin
- */
 const isAdmin = (username) => {
-  return Twitch.Config.admins.includes(username.toLowerCase());
+	return Twitch.Config.admins.includes(username.toLowerCase());
 };
 
 Twitch.Client.connect();
 Twitch.CurrentClash = {};
 Twitch.EventListeners = {
-  message: async function onMessage(channel, tags, message, self) {
-    const send = (msg) =>
-      Twitch.Client.say(channel, `@${tags.username} ` + msg);
-    // if(self) return;
-    // console.log(`Message: ${message}`);
-    // console.log(`Tags: `, tags);
+	message: async function onMessage(channel, tags, message, self) {
+		const send = (msg) =>
+			Twitch.Client.say(channel, `@${tags.username} ` + msg);
+		// if(self) return;
+		// console.log(`Message: ${message}`);
+		// console.log(`Tags: `, tags);
 
-    if (message.startsWith(`@${Twitch.Config.username} prefix`)) {
-      if (!isAdmin(tags.username))
-        return send(`Current prefix: ${Twitch.Config.prefix}`);
-      Twitch.Config.prefix = message.split(" ").last();
-      SaveConfig();
-      console.log(`[TWITCH] Prefix set, saving new config.json`);
-      return send(`Successfully set prefix to ${Twitch.Config.prefix}`);
-    }
+		if (message.startsWith(`@${Twitch.Config.username} prefix`)) {
+			if (!isAdmin(tags.username))
+				return send(`Current prefix: ${Twitch.Config.prefix}`);
+			Twitch.Config.prefix = message.split(' ').last();
+			SaveConfig();
+			console.log(`[TWITCH] Prefix set, saving new config.json`);
+			return send(`Successfully set prefix to ${Twitch.Config.prefix}`);
+		}
 
-    if (!message.startsWith(Twitch.Config.prefix)) return;
-    const _cmd = message.slice(Twitch.Config.prefix.length).split(" ");
-    const cmd = _cmd[0];
-    const args = _cmd.slice(1);
+		if (!message.startsWith(Twitch.Config.prefix)) return;
+		const _cmd = message.slice(Twitch.Config.prefix.length).split(' ');
+		const cmd = _cmd[0];
+		const args = _cmd.slice(1);
 
-    switch (cmd.toLowerCase()) {
-      /**
-       * request help
-       *
-       * @returns {string} a list of available commands
-       */
-      case "help":
-        let viewerCommands = [
-          "!help",
-          "!link",
-          "!uptime",
-          "!playlist",
-          "!followage",
-        ];
-        let adminCommands = ["!new", "!start", "!override", "!eval"];
+		switch (cmd.toLowerCase()) {
+			case 'help':
+				let viewerCommands = ['!help', '!link', '!uptime', '!playlist', '!followage',];
+				let adminCommands = ['!new', '!start', '!override', '!eval'];
 
-        if (isAdmin(tags.username))
-          return send(
-            `list of commands: ${[...viewerCommands, ...adminCommands].join(
-              ", "
-            )}`
-          );
+				if (isAdmin(tags.username))
+					return send(
+						`list of commands: ${[...viewerCommands, ...adminCommands].join(
+							', '
+						)}`
+					);
 
-        return send(`sup bru, looking for list of commands?
-		today is your lucky day: ${viewerCommands.join(", ")}`);
+				return send(`sup bru, looking for list of commands?
+		today is your lucky day: ${viewerCommands.join(', ')}`);
 
-      /**
-       * request the current clash link
-       *
-       * @returns {string} link to clash
-       */
-      case "link":
-        return send(Twitch.CurrentClash.url ?? "No clash created yet...");
+			case 'link':
+				return send(Twitch.CurrentClash.url ?? 'No clash created yet...');
 
-      /**
-       * create a new clash
-       *
-       * @returns {string} link to clash
-       */
-      case "new":
-        if (!isAdmin(tags.username)) return;
+			case 'new':
+				if (!isAdmin(tags.username)) return;
 
-        Twitch.CurrentClash = new CG.Clash(args, args);
-        if (!(await Twitch.CurrentClash.create()))
-          return send("Creating clash failed (see console)...");
-        return send(Twitch.CurrentClash.url);
+				Twitch.CurrentClash = new CG.Clash(args, args);
+				if (!(await Twitch.CurrentClash.create())) return send('Creating clash failed (see console)...');
+				return send(Twitch.CurrentClash.url);
 
-      /**
-       * start the current clash
-       *
-       * @returns {string} link to clash
-       */
-      case "start":
-        if (!isAdmin(tags.username)) return;
+			case 'start':
+				if (!isAdmin(tags.username)) return;
+				if (!Twitch.CurrentClash.start) return send(`There hasn't been a clash created.`);
 
-        if (!Twitch.CurrentClash.start)
-          return send(`There hasn't been a clash created.`);
+				await Twitch.CurrentClash.start();
+				if (CG.Config.autoSubmit)
+					setTimeout(() => Twitch.CurrentClash.submit(), 30000);
+				break;
 
-        await Twitch.CurrentClash.start();
-        if (CG.Config.autoSubmit)
-          setTimeout(() => Twitch.CurrentClash.submit(), 30000);
-        break;
+			case 'override':
+				if (!isAdmin(tags.username)) return;
 
-      /**
-       * override the current clash link
-       *
-       * @returns {string} overridden link
-       */
-      case "override":
-        if (!isAdmin(tags.username)) return;
+				if (args.length == 0) return send('No link provided, usage: !override <link>');
+				if (args.length > 1) return send('Too many arguments, usage: !override <link>');
+				Twitch.CurrentClash.url = args[0];
 
-        if (args.length == 0)
-          return send("No link provided, usage: !override <link>");
-        if (args.length > 1)
-          return send("Too many arguments, usage: !override <link>");
-        Twitch.CurrentClash.url = args[0];
+				return send(Twitch.CurrentClash.url);
 
-        return send(Twitch.CurrentClash.url);
+			case 'eval':
+				if (!Twitch.Config.evalEnabled) return console.log('Eval is disabled');
+				if (!Twitch.Config.evalGlobal && !isAdmin(tags.username)) return;
 
-      /**
-       * eval code
-       *
-       * @returns {string} output of code
-       */
-      case "eval":
-        if (!Twitch.Config.evalEnabled) return console.log("Eval is disabled");
-        if (!Twitch.Config.evalGlobal && !isAdmin(tags.username)) return;
-
-        let out = eval(args.join(" "));
-        if (out == null) out = "No output...";
-        if (typeof out == "object") out = JSON.stringify(out);
-        return send(out);
-    }
-  },
+				let out = eval(args.join(' '));
+				if (out == null) out = 'No output...';
+				if (typeof out == 'object') out = JSON.stringify(out);
+				return send(out);
+		}
+	},
 };
 
-Twitch.Client.on("message", Twitch.EventListeners.message);
+Twitch.Client.on('message', Twitch.EventListeners.message);
